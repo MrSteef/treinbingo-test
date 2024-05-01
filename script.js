bb6afc8ec7fe70bbff18b2e85b9e5f3c6a9d276e
Stefan Veltmaat <s.e.m.veltmaat@student.utwente.nl>
2024-05-01 22:43:52 +0200

    added context depending on distance from station

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ options = {
 };
 
 function savekey() {
-  let keyToSave = document.getElementById('apikey').value
-  if (keyToSave != null && keyToSave != undefined && keyToSave != '') {
-    localStorage.setItem('apikey', keyToSave)
-    console.log('key saved')
+  let keyToSave = document.getElementById("apikey").value;
+  if (keyToSave != null && keyToSave != undefined && keyToSave != "") {
+    localStorage.setItem("apikey", keyToSave);
+    console.log("key saved");
   } else {
-    console.log('key not saved')
+    console.log("key not saved");
   }
 }
 
 function loadkey() {
-  let keyToLoad = localStorage.getItem('apikey')
-  if (keyToLoad != null && keyToLoad != undefined && keyToLoad != '') {
-    document.getElementById('apikey').value = keyToLoad
-    console.log('key loaded')
+  let keyToLoad = localStorage.getItem("apikey");
+  if (keyToLoad != null && keyToLoad != undefined && keyToLoad != "") {
+    document.getElementById("apikey").value = keyToLoad;
+    console.log("key loaded");
   } else {
-    console.log('key not loaded')
+    console.log("key not loaded");
   }
 }
 
@@ -46,13 +46,18 @@ function success(pos) {
 }
 
 function error(err) {
-  console.error(err)
+  console.error(err);
   document.getElementById("error").textContent = err.message;
 }
 
 function refresh() {
-  if (latitude == null || latitude == undefined || longitude == null || longitude == undefined) {
-    return
+  if (
+    latitude == null ||
+    latitude == undefined ||
+    longitude == null ||
+    longitude == undefined
+  ) {
+    return;
   }
   fetch(
     `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations/nearest?lat=${latitude}&lng=${longitude}&limit=1`,
@@ -61,22 +66,37 @@ function refresh() {
       // Request headers
       headers: {
         "Cache-Control": "no-cache",
-        "Ocp-Apim-Subscription-Key": document.getElementById('apikey').value,
+        "Ocp-Apim-Subscription-Key": document.getElementById("apikey").value,
       },
     }
   )
     .then((response) => response.json())
-    .then(json => {
-      const station = json.payload[0]
-      console.log(station)
-      const naam = station.namen.middel
-      const afstand = station.distance
-      document.getElementById(
-        "station-name"
-      ).textContent = `station ${naam} op ${afstand} meter afstand`;
+    .then((json) => {
+      const station = json.payload[0];
+      const message = generateMessage(station);
+      document.getElementById("station-name").innerHTML = message;
       document.getElementById("error").textContent = "";
     })
     .catch((err) => {
-      error(err)
+      error(err);
     });
+}
+
+function generateMessage(station) {
+  if (station.distance < station.radius) {
+    return `Je bevindt je op station ${station.namen.middel}<br>
+station afstand: ${station.distance}<br>
+station radius: ${station.radius}<br>
+naderen radius: ${station.naderenRadius}`;
+  }
+  if (station.distance < station.naderenRadius) {
+    return `Je nadert station ${station.namen.middel}<br>
+station afstand: ${station.distance}<br>
+station radius: ${station.radius}<br>
+naderen radius: ${station.naderenRadius}`;
+  }
+  return `Het dichtstbijzijnde station is ${station.namen.middel}<br>
+station afstand: ${station.distance}<br>
+station radius: ${station.radius}<br>
+naderen radius: ${station.naderenRadius}`;
 }
